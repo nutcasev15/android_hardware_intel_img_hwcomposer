353fe7ac367a923d04337d7374c1702156650c0d
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-14 08:10:10 -0400

    intel: hwcomposer: Only use DRM_PSB_PM_SET if it exists
    Older devicds don't have this ioctl in their kernels and need to
    rely on the older calls.  Conditionally restore them after
    the merge of 6.0.1_r22.
    
    Change-Id: I9ac7b015b1d039095cbc84867ef8d7dc2a74324c

--- merrifield/common/devices/PhysicalDevice.cpp
+++ merrifield/common/devices/PhysicalDevice.cpp
@@ -529,6 +529,7 @@ uint32_t PhysicalDevice::getFpsDivider()
 
 bool PhysicalDevice::setPowerMode(int mode)
 {
+#ifdef DRM_PSB_PM_SET
     // TODO: set proper power modes for HWC 1.4
     ATRACE("mode = %d", mode);
 
@@ -543,6 +544,21 @@ bool PhysicalDevice::setPowerMode(int mode)
     }
 
     return true;
+#else
+    // TODO: set proper blanking modes for HWC 1.4 modes
+    switch (mode) {
+        case HWC_POWER_MODE_OFF:
+            return blank(true);
+        case HWC_POWER_MODE_DOZE:
+        case HWC_POWER_MODE_NORMAL:
+        case HWC_POWER_MODE_DOZE_SUSPEND:
+            return blank(false);
+        default:
+            return false;
+    }
+
+    return false;
+#endif
 }
 
 int PhysicalDevice::getActiveConfig()
